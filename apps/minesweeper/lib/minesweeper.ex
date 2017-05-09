33b9e03e065f486fcf2ee3a7d33686080580398b
Tim Huddle <blazhen88@gmail.com>
2017-05-09 10:54:59 -0400

    semi sure the business logic of Minesweeper functions, onto the UI!!!

--- apps/minesweeper/lib/minesweeper.ex
+++ apps/minesweeper/lib/minesweeper.ex
@@ -3,8 +3,8 @@ defmodule Minesweeper do
 
   use GenServer
 
-  @typep field_t :: [[boolean | non_neg_integer | :flag | :maybe_flag]]
-  @typep mines_t :: [[boolean]]
+  @typep field_t :: [[:blank | :flag | :maybe_flag | :mine | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8]]
+  @typep mines_t :: [[:blank | :mine]]
 
   @typedoc "The Minesweeper GenServer state"
   @type t :: %__MODULE__{
@@ -42,40 +42,42 @@ defmodule Minesweeper do
     GenServer.cast @gen_server_name, :stop
   end
 
-  @spec show() :: t
+  @spec show() :: :ok
   def show do
     GenServer.call @gen_server_name, :show
   end
 
-  @spec flag(integer, integer) :: t
+  @spec flag(integer, integer) :: field_t
   def flag(x, y) do
     GenServer.call @gen_server_name, {:flag, x, y}
   end
 
-  @spec pick(integer, integer) :: {:ok | :lose, t}
+  @spec pick(integer, integer) :: {:ok | :lose, field_t}
   def pick(x, y) do
     GenServer.call @gen_server_name, {:pick, x, y}
   end
 
-  def handle_call(:show, _from, state), do: {:reply, state, state}
+  def handle_call(:show, _from, state), do: {:reply, to_string(state), state}
   def handle_call({:flag, x, y}, _from, state) do
     update_fun = fn val ->
       case val do
         :flag -> :maybe_flag
-        :maybe_flag -> false
-        false -> :flag
-        x when is_integer x -> x
+        :maybe_flag -> :blank
+        :blank -> :flag
+        x -> x
       end
     end
     new_state = %{state | field: update_position_fun(state.field, x, y, update_fun)}
-    {:reply, new_state, new_state}
+    {:reply, new_state.field, new_state}
   end
   def handle_call({:pick, x, y}, _from, state) do
     case state.mines |> Enum.fetch!(y) |> Enum.fetch!(x) do
-      true -> {:reply, {:lose, state}, state}
-      false ->
-        new_state = uncover state
-        {:reply, {:ok, new_state}, new_state}
+      :mine ->
+        new_state = %{state | field: update_position(state.field, x, y, :mine)}
+        {:reply, {:lose, new_state.field}, new_state}
+      :blank ->
+        new_state = %{state | field: uncover(state.field, state.mines, x, y)}
+        {:reply, {:ok, new_state.field}, new_state}
     end
   end
 
@@ -91,7 +93,7 @@ defmodule Minesweeper do
   defp make_field(:medium), do: make_field @medium_size, @medium_size
   defp make_field(:large), do: make_field @large_width, @medium_size
   defp make_field(width, height) when width >= 9 and height >= 9 do
-    for _ <- 1..height, do: for _ <- 1..width, do: false
+    for _ <- 1..height, do: for _ <- 1..width, do: :blank
   end
 
   @small_count 10
@@ -107,10 +109,10 @@ defmodule Minesweeper do
   defp seed_field(field, max_height, max_width, mine_count) do
     y = Enum.random(1..max_height) - 1
     x = Enum.random(1..max_width) - 1
-    if field |> Enum.fetch!(y) |> Enum.fetch!(x) do
+    if field |> Enum.fetch!(y) |> Enum.fetch!(x) == :mine do
       seed_field field, max_height, max_width, mine_count
     else
-      updated_field = update_position field, x, y, true
+      updated_field = update_position field, x, y, :mine
       seed_field updated_field, max_height, max_width, mine_count - 1
     end
   end
@@ -128,8 +130,55 @@ defmodule Minesweeper do
     Enum.take(field, y) ++ [updated_row] ++ Enum.drop(field, y + 1)
   end
 
-  @spec uncover(t) :: t
-  defp uncover(state) do
-    state
+  @spec uncover(field_t, mines_t, integer, integer) :: field_t
+  defp uncover(field, mines, x, y) do
+    if field |> Enum.fetch!(y) |> Enum.fetch!(x) == :blank do
+      case compute_touch_count mines, x, y do
+        0 ->
+          new_field = update_position field, x, y, 0
+          max_y = Enum.count(mines) - 1
+          max_x = Enum.count(Enum.fetch! mines, 0) - 1
+          locations = for ix <- x - 1..x + 1,
+                          iy <- y - 1..y + 1,
+                          ix in 0..max_x,
+                          iy in 0..max_y, do: {ix, iy}
+          Enum.reduce locations, new_field, fn {x, y}, fld -> uncover fld, mines, x, y end
+        count ->
+          update_position field, x, y, count
+      end
+    else
+      field
+    end
   end
+
+  @spec compute_touch_count(mines_t, integer, integer) :: integer
+  defp compute_touch_count(mines, x, y) do
+    max_y = Enum.count(mines) - 1
+    max_x = Enum.count(Enum.fetch! mines, 0) - 1
+    locations = for ix <- x - 1..x + 1,
+                    iy <- y - 1..y + 1,
+                    ix in 0..max_x,
+                    iy in 0..max_y, do: {ix, iy}
+    Enum.reduce locations, 0, fn {x, y}, acc ->
+      if mines |> Enum.fetch!(y) |> Enum.fetch!(x) == :mine, do: acc + 1, else: acc end
+  end
+end
+
+defimpl String.Chars, for: Minesweeper do
+  def to_string(%Minesweeper{field: field, mines: mines}) do
+    field
+    |> Enum.zip(mines)
+    |> Enum.map_join("\n", fn {f, m} ->
+      Enum.join(Enum.map(f, &map_to_char/1) ++
+                [" ", " ", "|", " ", " "] ++
+                Enum.map(m, &map_to_char/1))
+    end)
+  end
+
+  defp map_to_char(:blank), do: "."
+  defp map_to_char(:mine), do: "*"
+  defp map_to_char(:flag), do: "⚑"
+  defp map_to_char(:maybe_flag), do: "?"
+  defp map_to_char(0), do: " "
+  defp map_to_char(x), do: to_string x
 end
